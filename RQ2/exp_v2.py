4818ecf8d008f5eef9607e7462573ccd6a29697a
DelaramGh <deli.zizou@gmail.com>
2024-05-24 14:07:06 -0400

    RQ2 exp_v2 added

--- RQ2/exp_v2.py
+++ RQ2/exp_v2.py
@@ -1,10 +1,19 @@
-from active_learning_v2 import active_labeling
+from active_learning_v3 import active_labeling
 import pandas as pd
 from tqdm import tqdm
 
-models = ["Random Forest", "Decision Tree", "SVM", "Logistic Regression"]
-inputs = [[], [], [], []] #cifar
-inputs = [[], [], [], []] #smartInside
+models = ["Decision Tree", "SVM", "Logistic Regression", "Random Forest"]
+
+inputs = [["KL"], 
+          ["Hist_int", "KL", "CPL", "CS"], 
+          ["Hist_int", "KL", "CPL", "CS"],
+          ["Hist_int", "KL", "CPL"]] #cifar
+
+# inputs = [["SSIM", "CS", "CPL"], 
+#           ["PSNR", "CS"], 
+#           ["PSNR", "CS", "CPL"],
+#           ["PSNR", "CS", "CPL"]] #smartInside
+
 thresholds = [0.8, 0.85, 0.9, 0.95, 0.99]
 split1 = [0.1, 0.15, 0.2, 0.25, 0.3, 0.4]
 split2 = [0.01, 0.03, 0.05, 0.08, 0.1, 0.15]
@@ -12,14 +21,14 @@ split2 = [0.01, 0.03, 0.05, 0.08, 0.1, 0.15]
 
 dataset = "..\\cifar dataset\\test_dataset.csv"
 
-for model in models:
+for model_number in range(len(models)):
     he = []
     accs = []
     params = []
     for th in tqdm(thresholds):
         for s2 in tqdm(split2):
             for s1 in split1:
-                human_effort, acc, param = active_labeling(dataset, model, th, split1=s1, split2=s2, print_=False)
+                human_effort, acc, param = active_labeling(dataset, models[model_number], inputs[model_number], th, split1=s1, split2=s2, print_=False)
                 he.append(human_effort)
                 accs.append(acc)
                 params.append(param)
@@ -30,7 +39,7 @@ for model in models:
     df = pd.DataFrame({"model":model_name, "threshold":threshold, 
                     "split_1":split_1, "split_2":split_2, 
                     "accuracy":accs, "human effort":he})
-    df.to_csv(f"results\\cifar_dataset\\exp2_d2_search__{model}.csv")
+    df.to_csv(f"results\\cifar_dataset\\exp2_d2_search__{models[model_number]}.csv")
 
 
 
